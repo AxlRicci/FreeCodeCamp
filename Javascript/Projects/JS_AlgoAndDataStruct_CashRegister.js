c19a514c1f9e88955e482d2779ba12158fb52997
AxlRicci <alexbricci@gmail.com>
2020-02-22 13:24:44 -0500

    cleaned JS_AlgoAndDataStruct_CashRegister.js file

--- Javascript/Projects/JS_AlgoAndDataStruct_CashRegister.js
+++ Javascript/Projects/JS_AlgoAndDataStruct_CashRegister.js
@@ -73,37 +73,3 @@ function checkCashRegister(price, cash, cid) {
         return "ERROR";
     }
 };
-//     // iterate through available funds to see if change can be made with CID.
-        
-// 		for (let i = 0; i < cidRev.length; i++){
-//             let denomCounter = 0;
-// 			if (change - cidRev[i][3] > -0.01){ // check if number can be subtracted using current number 
-// 				while (change - cidRev[i][3] > -0.01){ // subtract until number is gone or cannot be subtracted by current number anymore.
-//                     if (cidRev[i][1] == 0){ // if the amount of 
-//                         break;
-//                     }
-//                     change -= cidRev[i][3];
-//                     cidRev[i][1] -= cidRev[i][3];
-//                     denomCounter += cidRev[i][3];
-// 				}
-//                 reciept.change.push([cidRev[i][0],denomCounter]); 
-// 			} else {
-// 				continue; // if current number is too large to subtract will move on to next in list.
-// 			}
-//         }
-//         return reciept;
-//     }
-//   }
-  
-//   // Example cash-in-drawer array:
-//   // [["PENNY", 1.01],
-//   // ["NICKEL", 2.05],
-//   // ["DIME", 3.1],
-//   // ["QUARTER", 4.25],
-//   // ["ONE", 90],
-//   // ["FIVE", 55],
-//   // ["TEN", 20],
-//   // ["TWENTY", 60],
-//   // ["ONE HUNDRED", 100]]
-  
-console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
